e88fa60dd91d9181cb4e83043e21370837a1c48b
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-10-04 12:39:57 +0200

    add ts config

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import "./index.css";
 //   </StrictMode>
 // );
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
       <App />
